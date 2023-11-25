416ae3d1b73d3f5247a26266a0032b2e3975b490
zayani <zayani82@gmail.com>
2023-11-25 17:44:52 +0300

    Add files via upload

--- m/index.js
+++ m/index.js
@@ -5,11 +5,29 @@ console.log(memes);
 //get url params
 let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
-let mparms = params.get("parms");
+let mparms = params.get("parms").split(",");
 
-//set body inner html
-let body = document.querySelector("body");
-body.innerHTML = `
-<h1>${id}</h1>
-${mparms.split(",").join("<br>")}
-`;
+//set the src of the image #img
+let img = document.querySelector("#img");
+img.src = "./i/" + id + ".jpg";
+img.alt = id;
+
+//create mparms labels
+let pLabels = mparms
+  .map(
+    (lbl, i) =>
+      `<div class="label" style="${memes[id][i + 1]}"><div>${lbl}</div></div>`
+  )
+  .join("\n");
+
+//append pLabels after #img
+img.insertAdjacentHTML("afterend", pLabels);
+
+//get div #meme
+let meme = document.querySelector("#meme");
+
+//set style of #meme
+meme.style = memes[id][0]?.split("|")[0];
+
+//set class of #meme
+meme.className = memes[id][0]?.split("|")[1];
